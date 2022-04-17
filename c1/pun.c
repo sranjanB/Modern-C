a9e1de35b0dfbca99e2ccfe930d984f5dba04b55
sranjan <srbehura95@gmail.com>
2022-04-17 23:26:29 +0530

    added c2

---
+++ c1/pun.c
@@ -0,0 +1,10 @@
+/* Name: pun.c                */
+/* Purpose: Prints a bad pun. */
+/* Author: Not me             */
+#include <stdio.h>
+
+int main(void)
+{
+    printf("To C, or not to C: that is the question.\n");
+    return 0;
+}
